05c489a69dd595c94a01912b41424b0715ce9c9d
badrbelabrik <badr.belabrik@gmail.com>
2026-04-30 16:02:57 +0100

    change database port

--- includes/connection.php
+++ includes/connection.php
@@ -1,6 +1,6 @@
 <?php 
 try{
-    $conn = new PDO ("mysql:host=localhost:3307;dbname=school_db","root","");
+    $conn = new PDO ("mysql:host=localhost:3306;dbname=school_db","root","");
 } catch(PDOException $e){
     echo "connection failed: " . $e->getMessage();
 }
